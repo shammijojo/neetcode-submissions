7b1a50b270896a223ef8d506ed9de3af4dccb972
Shammi Jojo <72042840+shammijojo@users.noreply.github.com>
2026-08-21 17:49:05 +0530

    Add: first-missing-positive - submission-2

---
+++ Data Structures & Algorithms/first-missing-positive/submission-2.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+       boolean isOnePresent = false;
+
+    //    for(int i = 0; i < nums.length; i++) {
+    //         if(nums[i] == 1) {
+    //             isOnePresent = true;
+    //             break;
+    //         }
+    //    }
+
+    //    if(!isOnePresent) {
+    //         return 1;
+    //    }
+
+        int n = nums.length;
+       for(int i = 0; i < n; i++) {
+            if(nums[i] <= 0 || nums[i] > n) {
+                nums[i] = n+1;
+            }
+       }
+
+
+        
+        for(int i = 0; i < n; i++) {
+            if(Math.abs(nums[i]) > n) continue;
+            nums[Math.abs(nums[i])-1] = Math.abs(nums[Math.abs(nums[i])-1]) * -1;
+
+        }
+
+        for(int i = 0; i < n ; i++) {
+            if(nums[i] > 0) {
+                return i+1;
+            }
+        }
+
+        return n+1;
+
+
+    }
+}
