3a0643ee79478307d09a3177394d20752b6a364f
Shammi Jojo <72042840+shammijojo@users.noreply.github.com>
2026-08-24 16:02:00 +0530

    Add: car-pooling - submission-0

---
+++ Data Structures & Algorithms/car-pooling/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        List<int[]> points = new ArrayList<>();
+
+        for(int i = 0; i < trips.length; i++) {
+            points.add(new int[]{trips[i][1],trips[i][0]});
+            points.add(new int[]{trips[i][2],-trips[i][0]});
+        }
+
+        Collections.sort(points,(a,b) -> a[0]-b[0]);
+
+        int currentPassengers = 0;
+        for(int[] point : points) {
+            currentPassengers += point[1];
+            if(currentPassengers > capacity) {
+                return false;
+            }
+        }
+
+        return true;
+
+        
+    }
+}
